dca2889069b679209eb1b22fe656f2469e8d2930
Eric Neves Rodrigues <ericnevesrodrigues@gmail.com>
2023-03-08 06:24:16 -0400

    Changed name color

--- src/qrcode.js
+++ src/qrcode.js
@@ -4,10 +4,10 @@ const path = document.querySelector('canvas')
 
 const info = 'https://github.com/ericneves'
 
-const options = { 
-        dark: '#FFFFFF',  
-        light: '#0000' 
-    }
+const options = {
+    light: '#FFFFFF',
+    dark: '#0000'
+}
 
 QRCode.toDataURL(path, info, { color: options }, function (error) {
     if (error) console.error(error)
