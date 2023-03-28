f91dffd16fc3a16c66d61278c15b7da876ba8970
Eric Neves <ericnevesrodrigues@gmail.com>
2023-03-28 12:13:26 -0400

    Add form

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import QRCode from "qrcode"
 
 const path = document.querySelector('canvas')
-
-const info = 'https://github.com/EricNeves/QRCode'
+const form = document.forms.form 
 
 const options = {
     dark: '#FFFFFF',
     light: '#0000'
 }
 
-QRCode.toDataURL(path, info, { color: options }, function (error) {
+form.addEventListener('submit', function(e) {
+    e.preventDefault()
+    const info = form.info.value
+    
+    QRCode.toDataURL(path, info, { color: options }, function (error) {
+        if (error) console.error(error)
+    })
+    
+})
+
+QRCode.toDataURL(path, 'github.com/ericneves', { color: options }, function (error) {
     if (error) console.error(error)
 })
